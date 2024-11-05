85900a350b453e2f6a42d085a0f713fccb011e6c
vinhngn <vinhn.official@gmail.com>
2024-11-05 13:02:34 +0900

    Fixing loginUser func (#48)

--- frontend/src/services/AuthService.js
+++ frontend/src/services/AuthService.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:8080/auth'; // Update with your backend URL
 
-export const loginUser = async (userName, password) => {
-    const response = await axios.post(`${API_URL}/login`, { userName, password });
+export const loginUser = async (email, password) => {
+    const response = await axios.post(`${API_URL}/login`, { email, password });
     return response.data;
 };
 
