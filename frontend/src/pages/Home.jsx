a41f2a03ed888699f49bffb11b6c2264622acc7b
Thelilrex <137387550+Thelilrex@users.noreply.github.com>
2024-11-17 13:25:45 +0900

    added some basic bs

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,17 +5,36 @@ const Home = () => {
   return (
     <body className="d-flex flex-column min-vh-100 bg-danger">
       <div className="container">
+        <form className="d-flex" role="search">
+          <input
+            className="form-control me-2 bg-secondary"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn text-white bg-black" type="submit">
+            Search
+          </button>
+        </form>
         <Element_Page_Layout />
         <br />
         <br />
         <p>Expected structure</p>
         <p>Main Body</p>
-        <p>---header</p>
+        <p>
+          ---header <strong> Completed</strong>
+        </p>
         <p>---Body</p>
-        <p>------Search bar (future implimentation/ low priority)</p>
+        <p>
+          ------Search bar{" "}
+          <strong> Completed needs js for dictating the usage</strong>(future
+          implimentation/ low priority)
+        </p>
         <p>------rows div</p>
         <p>---------left row div (contains filters (future update))</p>
-        <p>---------right row div (contains elements)</p>
+        <p>
+          ---------right row div (contains elements) <strong> Completed</strong>
+        </p>
         <p>
           ------------elements (div) (Upon clicking this elements we should be
           redirected to elements page)
